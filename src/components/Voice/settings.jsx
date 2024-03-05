14a8016b99393ffc73cb3aed3e557b4c6c9b3b87
Taetiz1 <120712765+Taetiz1@users.noreply.github.com>
2024-03-05 12:54:19 +0700

    test

--- src/components/Voice/settings.jsx
+++ src/components/Voice/settings.jsx
@@ -1,10 +1,13 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
-    
+
+const appId = import.meta.env.AGORA_APPID
+const token = null
+
 export const appConfig = {
     mode: "rtc",
     codec: "vp8",
-    appId: process.env.AgoraAPPID,
-    token: null
+    appId: appId,
+    token: token
 }
 
 export const useVideoClient = createClient(appConfig);
